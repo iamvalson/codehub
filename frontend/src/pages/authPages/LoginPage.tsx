dab68aaddef7b25aa77d8986c0bbc70ec86132c6
iamvalson <itzvalsonwrld@gmail.com>
2026-03-11 19:05:26 +0100

    fix: update overflow class in LoginPage and SignupPage components

--- frontend/src/pages/authPages/LoginPage.tsx
+++ frontend/src/pages/authPages/LoginPage.tsx
@@ -4,7 +4,7 @@ import "material-symbols";
 
 const LoginPage = () => {
   return (
-    <div className="overflow-hidden h-screen flex font-display">
+    <div className="overflow-x-hidden h-screen flex font-display">
       <CodePreviewPanel />
       <section className="flex-1 flex flex-col items-center justify-center bg-background-dark px-4 py-8 sm:px-6 sm:py-12">
         <h1 className="md:hidden font-mono text-xl font-bold tracking-tighter mb-6 text-white">
